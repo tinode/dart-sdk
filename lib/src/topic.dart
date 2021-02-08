5ade11e214e704bda34aa2af778d893d527ab234
rxmoein <rxmoein@tuta.io>
2021-02-08 22:03:08 +0330

    Subscribe

--- lib/src/topic.dart
+++ lib/src/topic.dart
@@ -1,6 +1,72 @@
-/// FIXME: Implement `attachCacheToTopic` too
+import 'dart:async';
+
+import 'package:tinode/src/models/access-mode.dart';
+import 'package:tinode/src/models/topic-names.dart' as TopicNames;
+import 'package:tinode/src/services/tinode.dart';
+import 'package:get_it/get_it.dart';
+
+import 'models/get-query.dart';
+import 'models/set-params.dart';
+
+/// TODO: Implement `attachCacheToTopic` too
+
 class Topic {
-  Topic(String topicName) {}
+  bool _new;
+  String name;
+  AccessMode acs;
+  DateTime created;
+  DateTime updated;
+  bool _subscribed;
+  TinodeService _tinodeService;
+
+  Topic(String topicName) {
+    _resolveDependencies();
+    name = topicName;
+  }
+
+  void _resolveDependencies() {
+    _tinodeService = GetIt.I.get<TinodeService>();
+  }
+
+  // See if you have subscribed to this topic
+  bool get isSubscribed {
+    return _subscribed;
+  }
+
+  Future subscribe(GetQuery getParams, SetParams setParams) async {
+    // If the topic is already subscribed, return resolved promise
+    if (isSubscribed) {
+      return;
+    }
+
+    // Send subscribe message, handle async response.
+    // If topic name is explicitly provided, use it. If no name, then it's a new group topic, use "new".
+    var ctrl = await _tinodeService.subscribe(name != '' ? name : TopicNames.TOPIC_NEW, getParams, setParams);
+
+    if (ctrl['code'] >= 300) {
+      // Do nothing if the topic is already subscribed to.
+      return ctrl;
+    }
+
+    _subscribed = true;
+    acs = (ctrl['params'] != null && ctrl['params']['acs'] != null) ? ctrl['params']['acs'] : acs;
+
+    // Set topic name for new topics and add it to cache.
+    if (_new) {
+      _new = false;
+
+      // Name may change new123456 -> grpAbCdEf
+      name = ctrl.topic;
+      created = ctrl.ts;
+      updated = ctrl.ts;
+
+      if (name != TopicNames.TOPIC_ME && name != TopicNames.TOPIC_FND) {
+        // Add the new topic to the list of contacts maintained by the 'me' topic.
+        // TODO: Complete this method
+      }
+    }
+  }
+
   resetSub() {}
   allMessagesReceived(int count) {}
   processMetaSub(List<dynamic> a) {}
