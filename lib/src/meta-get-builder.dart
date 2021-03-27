bf4ed0546d9ed2f78d9a6722ff9b2b595a11cf74
rxmoein <rxmoein@tuta.io>
2021-03-27 16:29:13 +0430

    Meta get builder

--- lib/src/meta-get-builder.dart
+++ lib/src/meta-get-builder.dart
@@ -1,11 +1,22 @@
+import 'package:tinode/src/models/topic-names.dart' as topic_names;
+import 'package:tinode/src/services/tinode.dart';
+import 'package:tinode/src/topic-me.dart';
 import 'package:tinode/src/topic.dart';
+import 'package:get_it/get_it.dart';
 
 class MetaGetBuilder {
-  // FIXME: Implement
+  /// Tinode service, responsible for handling messages, preparing packets and sending them
+  TinodeService _tinodeService;
+
   String topic;
+  dynamic contact;
 
   MetaGetBuilder(Topic parent) {
+    _tinodeService = GetIt.I.get<TinodeService>();
+
     topic = parent.name;
+    TopicMe me = _tinodeService.getTopic(topic_names.TOPIC_ME);
+    contact = me != null ? me.getContact(parent.name) : null;
   }
 
   dynamic withLaterData(dynamic a) {}
--- lib/src/topic-me.dart
+++ lib/src/topic-me.dart
@@ -3,6 +3,7 @@ import 'package:tinode/src/topic.dart';
 class TopicMe extends Topic {
   TopicMe() : super('me');
 
+  dynamic getContact(dynamic a) {}
   getMsgReadRecv(String name, String what) {}
   setMsgReadRecv(String name, String what, int seq, DateTime ts) {}
 }
