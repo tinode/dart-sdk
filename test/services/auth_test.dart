3ecdf1519c1b19402ad0a0a60618f5570c424858
rxmoein <rxmoein@tuta.io>
2021-05-25 08:21:21 +0430

    Auth service onLoginSuccessful test

--- test/services/auth_test.dart
+++ test/services/auth_test.dart
@@ -1,6 +1,7 @@
 import 'package:test/test.dart';
 
 import 'package:tinode/src/models/auth-token.dart';
+import 'package:tinode/src/models/server-messages.dart';
 import 'package:tinode/src/services/auth.dart';
 
 void main() {
@@ -19,5 +20,20 @@ void main() {
   test('setUserId() should set userId', () {
     service.setUserId('test');
     expect(service.userId, equals('test'));
+  });
+
+  test('onLoginSuccessful() should set userId', () {
+    var expireDate = DateTime.now();
+    service.onLoginSuccessful(CtrlMessage(
+      code: 200,
+      params: {
+        'user': 'userTest',
+        'token': 'tokenTest',
+        'expires': expireDate.toIso8601String(),
+      },
+    ));
+    expect(service.userId, equals('userTest'));
+    expect(service.authToken.token, equals('tokenTest'));
+    expect(service.authToken.expires, equals(expireDate));
   });
 }
