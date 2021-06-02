7298b884c82b5af04c5b95c1afcb73ad10e6fceb
rxmoein <rxmoein@tuta.io>
2021-06-02 11:45:44 +0430

    Refactor DefAcs

--- lib/src/models/def-acs.dart
+++ lib/src/models/def-acs.dart
@@ -1,17 +1,14 @@
 /// Topic's default access permissions
 class DefAcs {
   /// Default access for authenticated users
-  String? auth;
+  String auth;
 
   /// Default access for anonymous users
-  String? anon;
+  String anon;
 
-  DefAcs({this.auth, this.anon});
+  DefAcs(this.auth, this.anon);
 
   static DefAcs fromMessage(Map<String, dynamic> msg) {
-    return DefAcs(
-      anon: msg['anon'],
-      auth: msg['auth'],
-    );
+    return DefAcs(msg['auth'], msg['anon']);
   }
 }
